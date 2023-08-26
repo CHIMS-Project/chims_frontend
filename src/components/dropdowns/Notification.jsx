8578ff790007316c1c13210590e1f6162abb0210
Benson Osei-Mensah <kbenson643@gmail.com>
2023-08-26 21:37:57 +0000

    Created component for dropdown content

--- src/components/dropdowns/Notification.jsx
+++ src/components/dropdowns/Notification.jsx
@@ -1,22 +1,23 @@
-import { faBell } from '@fortawesome/free-regular-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { AnimatePresence } from 'framer-motion';
-import { useState } from 'react'
-import { NotificationsDropdown } from './NotificationsDropdown';
+import { faBell } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { AnimatePresence } from "framer-motion";
+import { useState } from "react";
+import { NotificationsDropdown } from "./NotificationsDropdown";
+import { DropdownContainer } from "./DropdownContainer";
 
 export const Notification = () => {
-    const [showNotifications, setShowNotifications] = useState(false);
+	const [showNotifications, setShowNotifications] = useState(false);
 
-  return (
-    <div className="relative">
-    <button
-        onClick={() => setShowNotifications(!showNotifications)}
-        className="hover:bg-slate-100 px-2 py-1 rounded-md text-lg text-primary-500">
-        <FontAwesomeIcon icon={faBell} />
-    </button>
-    <AnimatePresence>
-        {showNotifications && <NotificationsDropdown />}
-    </AnimatePresence>
-</div>
-)
-}
+	return (
+		<DropdownContainer>
+			<button
+				onClick={() => setShowNotifications(!showNotifications)}
+				className="hover:bg-slate-100 px-2 py-1 rounded-md text-lg text-primary-500">
+				<FontAwesomeIcon icon={faBell} />
+			</button>
+			<AnimatePresence>
+				{showNotifications && <NotificationsDropdown />}
+			</AnimatePresence>
+		</DropdownContainer>
+	);
+};
--- src/components/dropdowns/UserHeaderOptions.jsx
+++ src/components/dropdowns/UserHeaderOptions.jsx
@@ -2,12 +2,14 @@ import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { AnimatePresence } from "framer-motion";
 import { UserMenuDropdown } from "./UserMenuDropdown";
-import { useState } from 'react'
+import { useState } from "react";
+import { DropdownContainer } from "./DropdownContainer";
+
 export const UserHeaderOptions = () => {
 	const [showDropdown, setShowDropdown] = useState(false);
 
 	return (
-		<div className="relative">
+		<DropdownContainer>
 			<button
 				onClick={() => setShowDropdown(!showDropdown)}
 				className="hover:bg-slate-100 px-2 py-1 rounded-md text-xs">
@@ -17,6 +19,6 @@ export const UserHeaderOptions = () => {
 			<AnimatePresence>
 				{showDropdown && <UserMenuDropdown />}
 			</AnimatePresence>
-		</div>
+		</DropdownContainer>
 	);
 };
