9f6a42f26ea7412bcc49733d0cccbfa524db664e
bensonOSei <kbenson643@gmail.com>
2023-09-05 19:14:41 +0000

    Added api calls to populate staffCount widget

--- src/serviceProviders/providers/UserProvider.jsx
+++ src/serviceProviders/providers/UserProvider.jsx
@@ -1,44 +1,17 @@
 import { UserContext } from "../contexts/UserContext";
-import PropTypes from 'prop-types';
-import { useCallback, useEffect, useState } from "react";
-import { getToken, isTokenSaved } from "../../utils/helpers";
-import { BACKEND_API_URL } from "../../utils/constants";
+import PropTypes from "prop-types";
+import { useState } from "react";
 
 export const UserProvider = ({ children }) => {
-    const [user, setUser] = useState(null)
+	const [user, setUser] = useState(null);
 
-    const fetchUser = useCallback(async (token) => {
-        const response = await fetch(`${BACKEND_API_URL}auth/`, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-                Accept: 'application/json',
-
-            }
-        })
-
-        const data = await response.json()
-
-        if (response.ok) {
-            setUser(data.user)
-            console.log(data.user)
-        } else {
-            setUser(null)
-        }
-
-    }, [])
-    useEffect(() => {
-        if(isTokenSaved() && user === null) {
-            const token = getToken()
-            fetchUser(token)
-        }
-    })
-    return (
-        <UserContext.Provider value={{user, setUser}}>
-            {children}
-        </UserContext.Provider>
-    )
-}
+	return (
+		<UserContext.Provider value={{ user, setUser }}>
+			{children}
+		</UserContext.Provider>
+	);
+};
 
 UserProvider.propTypes = {
-    children: PropTypes.node.isRequired
-}
+	children: PropTypes.node.isRequired,
+};
